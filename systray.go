d78b3154f1ca6c76f893195f7e77a596e658aa40
yzimhao <yzimhao@users.noreply.github.com>
2022-06-14 21:07:24 +0800

    Update systray.go

--- systray.go
+++ systray.go
@@ -2,6 +2,8 @@ package pinv
 
 import (
 	"os"
+	"os/exec"
+	"syscall"
 
 	"github.com/getlantern/systray"
 	"github.com/getlantern/systray/example/icon"
@@ -15,6 +17,14 @@ func systrayOnReady() {
 	systray.SetIcon(icon.Data)
 	systray.SetTitle(Options.AppName())
 	systray.SetTooltip(Options.AppName() + "超级棒")
+	mUrl := systray.AddMenuItem("Open", "open url")
+	go func() {
+		<-mUrl.ClickedCh
+		//windows
+		cmd := exec.Command("cmd", "/c", "start", "http://127.0.0.1:8080")
+		cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+		cmd.Start()
+	}()
 	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
 	go func() {
 		<-mQuit.ClickedCh
